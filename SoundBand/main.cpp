3d41b99c22a5cf3d55fd9a2551f93852d990cf8c
EndrII <EndrIIMail@gmail.com>
2018-08-21 21:02:52 +0300

    fix android size items

--- SoundBand/main.cpp
+++ SoundBand/main.cpp
@@ -4,9 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-#if defined(Q_OS_WIN)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
 
     QApplication app(argc, argv);
     App soundBand;
